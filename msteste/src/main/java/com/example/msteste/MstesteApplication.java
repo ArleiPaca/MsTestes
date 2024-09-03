76f1ab5c9fa044e89f3554c2efa196c3bdb9747a
Arlei Pacanaro Lepiani <EX300235@xs3.com.br>
2024-09-03 17:15:30 -0300

    first commit

---
+++ msteste/src/main/java/com/example/msteste/MstesteApplication.java
@@ -0,0 +1,13 @@
+package com.example.msteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstesteApplication.class, args);
+	}
+
+}
---
+++ msteste/src/test/java/com/example/msteste/MstesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
